92dff13f600ba21d1ce4e2e69c47e02ff645d759
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2025-03-28 13:05:20 -0300

    update sidebar form

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,18 +1,28 @@
-import { useEffect } from "react";
 import useWeather from "../hooks/useWeather";
 
 export default function Weather() {
   const city = "London";
   const { data, loading, error } = useWeather(city);
 
-  useEffect(() => {
-    console.log(data, loading, error);
-  }, [data, loading, error]);
-
   return (
     <>
-      <h1 className="text-2xl font-bold text-center pt-4">Weather App</h1>
-      <p className="text-center">Get the latest weather updates!</p>
+      <div id="weather" className="bg-red-500">
+        <p className="p-4">
+          Locación buscada: <span id="location"></span>
+        </p>
+        <p className="p-4">
+          Temperatura: <span id="temp"></span> °C
+        </p>
+        <p className="p-4">
+          Condición: <span id="condition"></span>
+        </p>
+        <p className="p-4">
+          Humedad: <span id="humidity"></span>%
+        </p>
+        <p className="p-4">
+          Velocidad del viento: <span id="wind"></span> km/h
+        </p>
+      </div>
     </>
   );
 }
--- src/shared/Sidebar.jsx
+++ src/shared/Sidebar.jsx
@@ -1,19 +1,54 @@
+import { useState } from "react";
+
 export default function Sidebar() {
+  const [location, setLocation] = useState("");
+
+  const searchWeather = (e) => {
+    e.preventDefault();
+    console.log(location);
+  };
+
   return (
     <nav className="bg-amber-500 text-white p-4 h-full md:min-w-[300px] flex flex-col justify-between">
       <h1 className="text-2xl text-center pb-8">Weather App</h1>
-      <ul className="flex-1">
-        <li className="py-2">opciones</li>
-        <li className="py-2">opciones</li>
-        <li className="py-2">opciones</li>
-      </ul>
+
+      <form className="flex-1 flex flex-col gap-4">
+        <label htmlFor="location" className="font-semibold text-lg">
+          Ingrese una locación
+        </label>
+        <input
+          id="location"
+          name="location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          type="text"
+          placeholder="Ingrese una locación"
+          className="bg-white rounded w-full p-2 text-black focus:outline-none"
+        />
+
+        <button
+          onClick={searchWeather}
+          type="submit"
+          className="bg-[#7D2D87] text-white p-2 rounded font-semibold transition duration-400 hover:bg-[#5E1F6D] cursor-pointer"
+        >
+          Buscar
+        </button>
+      </form>
+
       <div className="flex flex-col gap-3">
+        <a
+          href="https://www.figma.com/design/tAipfyfEFnpVb7bOO5etNf/Curriculum-Vitae?node-id=0-1&p=f"
+          target="_blank"
+          className="text-black bg-white p-2 rounded font-semibold transition duration-400 hover:text-white hover:bg-black"
+        >
+          Perfil Github
+        </a>
         <a
           href="https://github.com/matiasCoco1997"
           target="_blank"
           className="text-[#7D2D87] bg-white p-2 rounded font-semibold transition duration-400 hover:text-white hover:bg-[#7D2D87]"
         >
-          Perfil Github
+          Curriculum Vitae
         </a>
         <a
           href="https://www.linkedin.com/in/matias-coco-812a41230/"
